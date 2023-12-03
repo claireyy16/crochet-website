31ffb1e3bfa193a062d3f626eb8231a1b9e611be
claireyy16 <81452928+claireyy16@users.noreply.github.com>
2023-12-03 18:00:55 -0500

    format home page

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,59 +1,69 @@
-import React, { createContext, useState } from 'react';
-import { PRODUCTS } from '../products';
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../products";
 
 export const ShopContext = createContext(null);
 //create somewhere to keep track of states that need to be accessed everywhere
 
 const getDefaultCart = () => {
-	let cart = {}
-	for (let i = 1; i <PRODUCTS.length + 1; i++) {
+	let cart = {};
+	for (let i = 1; i < PRODUCTS.length + 1; i++) {
 		cart[i] = 0;
 	}
 	return cart;
-}
+};
 
 export const ShopContextProvider = (props) => {
-		const [cartItems, setCartItems] = useState(getDefaultCart());
-		console.log(cartItems);
+	const [cartItems, setCartItems] = useState(getDefaultCart());
+	console.log(cartItems);
 
-		const getTotalCartAmount = () => {
-			let totalAmount = 0;
-			for (const item in cartItems) {
-			{/*loop through items in cartItems*/}
-				if (cartItems[item] > 0) {
-					{/*if there is one of that item in cart*/}
-					let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
-					{/* get the id of that product and convert to a number */}
-					totalAmount += cartItems[item] * itemInfo.price;
-				}
+	const getTotalCartAmount = () => {
+		let totalAmount = 0;
+		for (const item in cartItems) {
+			{
+				/*loop through items in cartItems*/
 			}
-			console.log(totalAmount);
+			if (cartItems[item] > 0) {
+				{
+					/*if there is one of that item in cart*/
+				}
+				let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+				{
+					/* get the id of that product and convert to a number */
+				}
+				totalAmount += cartItems[item] * itemInfo.price;
+			}
+		}
+		console.log(totalAmount);
 
-			return totalAmount;
-		};
+		return totalAmount;
+	};
 
-		const addToCart = (itemID) => {
-			setCartItems((prev) => ({...prev, [itemID] : prev[itemID] + 1}));
-			//adding one more value to the item id
-		};
+	const addToCart = (itemID) => {
+		setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] + 1 }));
+		//adding one more value to the item id
+	};
 
-		const removeFromCart = (itemID) => {
-			setCartItems((prev) => ({...prev, [itemID] : prev[itemID] - 1}));
-			//removing one more value from the item id
-		};
+	const removeFromCart = (itemID) => {
+		setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }));
+		//removing one more value from the item id
+	};
 
-		const updateCartItemCount = (newAmount, itemID) => {
-			setCartItems((prev) => ({...prev, [itemID] : newAmount}));
-		};
+	const updateCartItemCount = (newAmount, itemID) => {
+		setCartItems((prev) => ({ ...prev, [itemID]: newAmount }));
+	};
 
-		const contextValue = {
-			cartItems,  
-			addToCart, 
-			removeFromCart, 
-			updateCartItemCount, 
-			getTotalCartAmount,
-		};
-		//object to pass in functions and values to access
+	const contextValue = {
+		cartItems,
+		addToCart,
+		removeFromCart,
+		updateCartItemCount,
+		getTotalCartAmount,
+	};
+	//object to pass in functions and values to access
 
-	return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
-}
+	return (
+		<ShopContext.Provider value={contextValue}>
+			{props.children}
+		</ShopContext.Provider>
+	);
+};
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,23 +5,23 @@ export const Home = () => {
 	return (
 		<div className="home">
 			<h1 className="title">home</h1>
-			<div className="textIntro">
-				<p className="intro">
+			<p className="intro">
 					{" "}
 					hello, welcome to my shop and my page!<br></br>
 					here is a bit about me:
 				</p>
+			<div className="textIntro">
 				<ul className="meList">
 					<li>i am a young adult cancer survivor and advocate</li>
 					<li>i am an undergraduate computer science student</li>
 					<li>i love creating, especially crocheting</li>
 				</ul>
-				<p>
+				<p className='about'>
 					i initially learned to crochet after looking at my friend's request
-					for a mushroom bucket hat and saying "that looks easy". <br></br> 
-          it was not easy. <br></br>
-          but, i found something i could use to pass time in the hospital,
-					and before long i was in love.
+					for a mushroom bucket hat and saying "that looks easy". <br></br>
+					it was not easy. <br></br>
+					but, i found something i could use to pass time in the hospital, and
+					before long i was in love.
 				</p>
 			</div>
 		</div>
